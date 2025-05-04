52e9da04079eb8d28e542cb360c03ca445986bec
Ankit Yadav <64765952+iankityadav@users.noreply.github.com>
2025-05-04 13:09:40 +0530

    asset prefix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
-  assetPrefix: "https://iankityadav.github.io/",
+  assetPrefix: isProd ? `https://${path.basename(__dirname)}` : "",
   images: {
     unoptimized: true,
   },
