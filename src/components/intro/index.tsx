a42941d372c33ce8ea58d6065f9b48f36ace9971
Ankit Yadav <64765952+iankityadav@users.noreply.github.com>
2025-05-11 14:07:11 +0530

    user icon updated

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -5,7 +5,7 @@ import IntroRight from "./right";
 const Intro = () => {
   return (
     <div className="flex flex-col md:flex-row items-center max-w-[90vw] mx-auto p-6 bg-gray-100 rounded-lg shadow-lg">
-      <div className="grid-cols-2 grid">
+      <div className="lg:grid-cols-2 grid grid-cols-1">
         <IntroLeft />
         <IntroRight />
       </div>
--- src/components/intro/right.tsx
+++ src/components/intro/right.tsx
@@ -1,42 +1,61 @@
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const IntroRight = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center">
         <div className="relative">
-          <div className="border-r-2 border-yellow-500 rounded-full p-8">
-            <Image
-              alt="user"
-              src={"/images/user-vector.png"}
-              width={400}
-              height={400}
-            />
+          <div className="absolute right-8 top-0">
+            <div className="w-[400px] h-[400px] relative mx-auto mt-10">
+              <svg
+                viewBox="0 0 400 400"
+                className="w-full h-full rotate-90 origin-center"
+              >
+                <path
+                  d="M 0,200 A 200,200 0 0,1 400,200"
+                  fill="none"
+                  stroke="var(--color-yellow-400)"
+                  strokeWidth="4"
+                />
+              </svg>
+
+              <div className="w-4 h-4 bg-yellow-500 rounded-full absolute top-0 right-1/2 transform -translate-y-1/2"></div>
+              <div className="w-4 h-4 bg-yellow-500 rounded-full absolute bottom-0 right-1/2 transform translate-y-1/2"></div>
+            </div>
           </div>
-          <div className="absolute inset-0 top-0 right-0 flex flex-col items-center rounded-full">
-            <div className="bg-gray-800 rounded-full p-2 absolute top-0 right-1/12 transform -translate-x-1/2">
+          <div className="rounded-full p-8 ">
+            <div
+              style={{
+                backgroundImage: "url(/images/user-vector-1.png)",
+                backgroundSize: "100%",
+              }}
+              className="w-[400px] h-[400px] block rounded-full"
+            >
+              .
+            </div>
+          </div>
+          <div className="absolute inset-0 top-8 right-3 flex flex-col items-center rounded-full text-2xl">
+            <div className="bg-gray-800 rounded-full p-2 absolute top-8 right-1/12 transform -translate-x-1/2">
               <Link href="/" className="text-white">
                 🏠
               </Link>
             </div>
-            <div className="bg-gray-800 rounded-full p-2 mt-2 absolute top-1/6 right-0">
+            <div className="bg-gray-800 rounded-full p-2 mt-2 absolute top-24 right-3">
               <Link href="/" className="text-white">
                 🙋‍♂️
               </Link>
             </div>
-            <div className="bg-gray-800 rounded-full p-2 mt-2 absolute top-2/6 -right-1/12 transform translate-y-1/2">
+            <div className="bg-gray-800 rounded-full p-2 mt-2 absolute top-5/12 right-0 transform ">
               <Link href="/" className="text-white">
                 📁
               </Link>
             </div>
-            <div className="bg-gray-800 rounded-full p-2 mt-2 absolute top-4/6 right-0 transform">
+            <div className="bg-gray-800 rounded-full p-2 mt-2 absolute bottom-28 right-4 transform">
               <Link href="/" className="text-white">
                 💬
               </Link>
             </div>
-            <div className="bg-gray-800 rounded-full p-2 mt-2 absolute top-5/6 right-3/12 transform">
+            <div className="bg-gray-800 rounded-full p-2 mt-2 absolute bottom-10 right-1/6 transform">
               <Link href="/" className="text-white">
                 📧
               </Link>
